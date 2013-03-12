eca28434742c5d838f6329d90e8fb234eb447f21
guillaume <guillaume.coutable@gmail.com>
2013-03-12 09:13:27 +0100

    Add Model

---
+++ src/model/Task.java
@@ -0,0 +1,50 @@
+package model;
+
+/**
+ * 
+ * @author guillaume
+ *
+ */
+public class Task {
+
+	private String taskName;
+	private boolean checked;
+	
+	/**
+	 * Task Contructor
+	 * @param name the name of the task
+	 */
+	public Task(String name) {
+		taskName = name;
+		checked = false;
+	}
+	
+	/**
+	 * @return the taskName
+	 */
+	public String getTaskName() {
+		return taskName;
+	}
+
+	/**
+	 * @return whether the task is checked or not
+	 */
+	public boolean isChecked() {
+		return checked;
+	}
+
+	/**
+	 * @param taskName the taskName to set
+	 */
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	/**
+	 * @param checked the new task state
+	 */
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
+
+}
