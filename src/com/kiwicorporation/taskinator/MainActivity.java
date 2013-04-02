29c130326419441cc8301b0a50399ce8bd24cf03
Nomyx <r.noemie25@orange.fr>
2013-04-02 16:01:59 +0200

    First serious push --> Doesn't work

--- src/com/kiwicorporation/taskinator/MainActivity.java
+++ src/com/kiwicorporation/taskinator/MainActivity.java
@@ -1,195 +1,45 @@
 package com.kiwicorporation.taskinator;
 
-import java.util.Locale;
+import java.util.ArrayList;
 
-import android.app.ActionBar;
-import android.app.FragmentTransaction;
+import controleur.ListManager;
+
+import model.ListT;
+import model.Task;
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 
-public class MainActivity extends FragmentActivity implements
-ActionBar.TabListener {
+public class MainActivity extends Activity {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+	private ExpandableListView allListView = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Set up the action bar.
-		final ActionBar actionBar = getActionBar();
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		// We take the expandable list of the XML
+		allListView = (ExpandableListView) findViewById(R.id.idAllList);
 
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-		// When swiping between different sections, select the corresponding
-		// tab. We can also use ActionBar.Tab#select() to do this if we have
-		// a reference to the Tab.
-		mViewPager
-		.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-			@Override
-			public void onPageSelected(int position) {
-				actionBar.setSelectedNavigationItem(position);
+		
+		for (int i = 1; i < 4; i++) {
+			ListT list = new ListT("Liste " + i);
+			ArrayList<Task> donnees = new ArrayList<Task>();
+			for (int x = 1; x < 4; x++) {
+				list.addTask(new Task("Task "+x));
 			}
-		});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter. Also specify this Activity object, which implements
-			// the TabListener interface, as the callback (listener) for when
-			// this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setIcon(mSectionsPagerAdapter.getPageIcon(i))
-					.setText(mSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
+			ListManager.getInstance().addList(list);
 		}
-
-//		android.app.FragmentManager fragmentManager = getFragmentManager();
-//		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-	}
-
-	@Override
-	public void onTabSelected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-		// When the given tab is selected, switch to the corresponding page in
-		// the ViewPager.
-		mViewPager.setCurrentItem(tab.getPosition());
-	}
-
-	@Override
-	public void onTabUnselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 2;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			Locale l = Locale.getDefault();
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase(l);
-			case 1:
-				return getString(R.string.title_section2).toUpperCase(l);
-			}
-			return null;
-		}
-
-		public int getPageIcon(int position){
-			switch (position){
-			case 0:
-				return R.drawable.list;
-			case 1:
-				return R.drawable.task;
-			}
-			return (Integer) null;
-		}
-	}
-
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = null;
-			System.out.println("Arg: "+getArguments().getInt(ARG_SECTION_NUMBER));
-			switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
-			case 0:
-				rootView = inflater.inflate(R.layout.list_fragment, container, false);
-				break;
-			case 1:
-				rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-				break;
-			}
-			//			return null;
-			//			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
-			//					container, false);
-			//			TextView dummyTextView = (TextView) rootView
-			//					.findViewById(R.id.section_label);
-			//			dummyTextView.setText(Integer.toString(getArguments().getInt(
-			//					ARG_SECTION_NUMBER)));
-			return rootView;
-		}
 	}
 
 }
